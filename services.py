80fdd1a9d30cd27754405460b3706930b7a16f76
egraich <egorbabchuk1@gmail.com>
2026-07-12 20:15:51 +0300

    services.py structure

---
+++ services.py
@@ -0,0 +1,6 @@
+async def extract_url(text: str) -> str | None :
+    return
+async def scan_url_virustotal(url: str) -> dict :
+    return
+async def get_ai_verdict(text: str, vt_context: str) -> str :
+    return
